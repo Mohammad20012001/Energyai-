6480650658a9da8dc5f57ece6a9d09d4d57e919e
Abulehyeh- ابولحيه <mohammadabulehyeh96@gmail.com>
2025-09-09 11:21:52 +0000

    404 This page could not be found. ما زالت قم بالغاء وجودها من الموقع واع

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import DashboardLayout from "./(dashboard)/layout";
 import DashboardOverviewPage from "./(dashboard)/page";
 
 export default function HomePage() {
   return (
-    <DashboardLayout>
-      <DashboardOverviewPage />
-    </DashboardLayout>
+    <DashboardOverviewPage />
   );
 }
